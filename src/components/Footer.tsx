14bd4ce907fde09af7ede26aba1792fa9c060397
unknown <hilyalansyaqia@gmail.com>
2026-03-16 13:59:14 +0700

    migrasi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: Github, href: '#', label: 'https://github.com/hilyalansyaqia-source/hilya-lansyaqia-X3.git' },
+    { icon: Github, href: 'https://github.com/hilyalansyaqia-source/', label: 'github' },
     // { icon: Linkedin, href: '#', label: 'LinkedIn' },
     // { icon: Youtube, href: '#', label: 'YouTube' },
     // { icon: Instagram, href: '#', label: 'Instagram' },
